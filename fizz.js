3dded1bc91094df172a719facc30294d325a6b66
arjeidi <robs.msn@gmail.com>
2018-01-03 23:30:58 -0500

    Initial commit

---
+++ fizz.js
@@ -0,0 +1,19 @@
+
+
+
+
+for (i=1;i<=20;++1)
+	{
+		if (i%3===0) && (i%5===0)
+		{
+			console.log(i + ": FizzBuzz")
+		}
+		else if (i%3===0)
+		{
+			console.log(i + ": Fizz")
+		}
+		else if (i%5===0)
+		{
+			console.log(i + ": Buzz")
+		}
+	}
