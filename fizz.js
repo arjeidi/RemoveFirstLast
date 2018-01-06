56d9587205bced050072f775f9ce4fec9ed10be3
arjeidi <robs.msn@gmail.com>
2018-01-06 18:58:54 -0500

    Fizz.js implemented and tested.

--- fizz.js
+++ fizz.js
@@ -1,19 +1,18 @@
 
 
 
-
-for (i=1;i<=20;++1)
-	{
-		if (i%3===0) && (i%5===0)
+	for (i=1;i<=20;++i)
 		{
-			console.log(i + ": FizzBuzz")
+			if (i%3===0 && i%5===0)
+			{
+				console.log(i + ": FizzBuzz")
+			}
+			else if (i%3===0)
+			{
+				console.log(i + ": Fizz")
+			}
+			else if (i%5===0)
+			{
+				console.log(i + ": Buzz")
+			}
 		}
-		else if (i%3===0)
-		{
-			console.log(i + ": Fizz")
-		}
-		else if (i%5===0)
-		{
-			console.log(i + ": Buzz")
-		}
-	}
